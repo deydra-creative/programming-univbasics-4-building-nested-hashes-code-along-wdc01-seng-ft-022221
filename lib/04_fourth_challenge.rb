d52f45dac324f4115d388dae0fa22ab2d04e1cb0
Deydra Cavazos <deydra@cavazospartners.com>
2021-02-07 02:48:01 +0000

    Automatically backed up by Learn

--- lib/04_fourth_challenge.rb
+++ lib/04_fourth_challenge.rb
@@ -6,8 +6,9 @@ def fourth_challenge
       :hero => {name: "Romeo", age: "15", status: "alive"},
       :hero_friends => [
         {name: "Benvolio", age: "17", attitude: "worried"}
-        {name: "Mercutio", age: "18", attitude: "hot-headed"} ]
-      end
+        {name: "Mercutio", age: "18", attitude: "hot-headed"} 
+        ]
+   },
 
    :capulet => {
       :patriarch => {name: "Lord Capulet", age: "50"},
@@ -16,9 +17,7 @@ def fourth_challenge
       :heroine_friends => [
         {name: "Steven", age: "30", attitude: "confused"}
         {name: "Nurse", age: "44", attitude: "worried"}
-     end
+   ]
+   }
+  }
   
-
-  
-
-end
